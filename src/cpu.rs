1d4dae44c26c5d5deb1f76b1ac0e0eeb87c1e7fd
me <me@me>
2022-08-04 14:37:55 +0800

    code refactor

--- src/cpu.rs
+++ src/cpu.rs
@@ -56,21 +56,55 @@ impl CPU {
         }
     }
 
+    fn mem_read(&self, addr: u16) -> u8 {
+        self.memory[addr as usize]
+    }
+
+    fn mem_write(&mut self, addr: u16, data: u8) {
+        self.memory[addr as usize] = data;
+    }
+
+    fn mem_read_u16(&mut self, pos: u16) -> u16 {
+        let lo = self.mem_read(pos) as u16;
+        let hi = self.mem_read(pos + 1) as u16;
+        (hi << 8) | (lo as u16)
+    }
+
+    fn mem_write_u16(&mut self, pos: u16, data: u16) {
+        let hi = (data >> 8) as u8;
+        let lo = (data & 0xff) as u8;
+        self.mem_write(pos, lo);
+        self.mem_write(pos + 1, hi);
+    }
+
+    pub fn load(&mut self, program: Vec<u8>) {
+        self.memory[0x8000..(0x8000 + program.len())].copy_from_slice(&program[..]);
+        self.mem_write_u16(0xFFFC, 0x8000);
+    }
+
+    pub fn reset(&mut self) {
+        self.register_a = 0;
+        self.register_x = 0;
+        self.status = 0;
+
+        self.program_counter = self.mem_read_u16(0xFFFC);
+    }
+
     fn update_zero_flag(&mut self, result: u8) {
         if result == 0 {
             //设置标志位为1
-            self.status = self.status | 0b0000_0010
+            self.status = self.status | 0b0000_0010;
         } else {
             //保留除标志位的其他位
-            self.status = self.status & 0b1111_1101
+            self.status = self.status & 0b1111_1101;
         }
     }
 
     fn update_negative_flag(&mut self, result: u8) {
         if result & 0b1000_0000 != 0 {
-            self.status = self.status | 0b1000_0000
+            self.status = self.status | 0b1000_0000;
         } else {
-            self.status = self.status & 0b0111_1111
+            self.status = self.status & 0b0111_1111;
         }
     }
 
@@ -101,19 +135,29 @@ impl CPU {
         self.update_negative_flag(self.register_a);
     }
 
-    pub fn interpret(&mut self, program: Vec<u8>) {
+    fn inx(&mut self) {
+        //INcrement X
+        self.register_x = self.register_x.wrapping_add(1);
+        self.update_zero_flag(self.register_x);
+        self.update_negative_flag(self.register_x);
+    }
+
+    pub fn run(&mut self) {
         loop {
-            let opscode = program[self.program_counter as usize];
+            let opscode = self.mem_read(self.program_counter);
             self.program_counter += 1;
             // http://www.6502.org/tutorials/6502opcodes.html
             match opscode {
+                0xE8 => {
+                    self.inx();
+                }
                 0xAA => {
                     self.tax();
                 }
                 0xA9 => {
                     //Mode: Immediate
                     //读取opscode的下一条参数
-                    let param = program[self.program_counter as usize];
+                    let param = self.mem_read(self.program_counter);
                     self.program_counter += 1;
                     self.lda(param);
                 }
@@ -124,6 +168,12 @@ impl CPU {
             }
         }
     }
+
+    pub fn load_and_run(&mut self, program: Vec<u8>) {
+        self.load(program);
+        self.reset();
+        self.run();
+    }
 }
 
 #[cfg(test)]
@@ -133,7 +183,7 @@ mod test {
     #[test]
     fn test_0xa9_lda_zero_flag() {
         let mut cpu = CPU::new();
-        cpu.interpret(vec![0xa9, 0x00, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0x00, 0x00]);
         assert_eq!(cpu.status & 0b0000_0010, 0b10);
     }
 
@@ -142,7 +192,7 @@ mod test {
     #[test]
     fn test_0xa9_lda_immediate_load_data() {
         let mut cpu = CPU::new();
-        cpu.interpret(vec![0xa9, 0x87, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0x87, 0x00]);
         assert_eq!(cpu.register_a, 0x87);
         assert_eq!(cpu.status & 0b0000_0010, 0b00);
         //0x87 - zero flag 后应该为 1000 0101
@@ -154,9 +204,22 @@ mod test {
     #[test]
     fn test_0xaa_tax_move_a_to_x() {
         let mut cpu = CPU::new();
+        cpu.load(vec![0xaa, 0x00]);
         cpu.register_a = 10;
-        cpu.interpret(vec![0xaa, 0x00]);
+        cpu.program_counter = cpu.mem_read_u16(0xFFFC);
+        cpu.run();
 
         assert_eq!(cpu.register_x, 10)
+    }
+
+    #[test]
+    fn test_inx_overflow() {
+        let mut cpu = CPU::new();
+        cpu.load(vec![0xe8, 0xe8, 0x00]);
+        cpu.register_x = 0xff;
+        cpu.program_counter = cpu.mem_read_u16(0xFFFC);
+        cpu.run();
+
+        assert_eq!(cpu.register_x, 1)
     }
 }
