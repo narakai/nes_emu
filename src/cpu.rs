dc3fe6c0849618d21ce7350cc91465870742aa62
me <me@me>
2022-08-05 15:30:43 +0800

    add addressing modes

--- src/cpu.rs
+++ src/cpu.rs
@@ -43,6 +43,35 @@ pub struct CPU {
     memory: [u8; 0xFFFF],
 }
 
+// https://www.jianshu.com/p/579682bb2f9c
+// https://www.jianshu.com/p/b909689db484
+#[derive(Debug)]
+#[allow(non_camel_case_types)]
+pub enum AddressingMode {
+    // 直接在指令中指定一个8位常量
+    Immediate,
+    // 使用零页寻址模式的指令只有一个8位地址操作数。这限制了它只能寻址前256字节的内存
+    ZeroPage,
+    // 使用零页，X寄存器寻址时，指令将8位零页地址和X寄存器中加载的值进行累加运算，得到的数据就是直接寻址的地址
+    ZeroPage_X,
+    // 使用零页，Y寄存器寻址时，指令将8位零页地址和Y寄存器中加载的值进行累加运算，得到的数据就是直接寻址的地址
+    ZeroPage_Y,
+    // 使用绝对寻址的指令需要包含一个完整的16位地址来识别目标位置
+    Absolute,
+    // 使用X寄存器索引进行绝对寻址访问地址的方法是通过s指令获取16位地址与X寄存器的中内容累加来计算得到的
+    Absolute_X,
+    // 将Y寄存器内存储的数据与一个16位地址相加，从而得到需要访问的地址
+    Absolute_Y,
+    // http://www.emulator101.com/6502-addressing-modes.html
+    // 从指令中获取表的地址，并向其添加X寄存器(零页循环)，以给出目标地址的最低有效字节的位置
+    // 索引间接寻址（Indexed Indirect）
+    Indirect_X,
+    // 在指令中包含16位地址的最低有效字节的零页位置。Y寄存器被动态添加到这个值中，以生成操作的实际目标地址。
+    // 间接变址寻址（Indirect Indexed）
+    Indirect_Y,
+    NoneAddressing,
+}
+
 impl CPU {
     pub fn new() -> Self {
         CPU {
@@ -56,6 +85,67 @@ impl CPU {
         }
     }
 
+    fn get_operand_address(&self, mode: &AddressingMode) -> u16 {
+        match mode {
+            AddressingMode::Immediate => {
+                self.program_counter
+            }
+            AddressingMode::ZeroPage => {
+                self.mem_read(self.program_counter) as u16
+            }
+            AddressingMode::Absolute => {
+                self.mem_read_u16(self.program_counter)
+            }
+            AddressingMode::ZeroPage_X => {
+                let pos = self.mem_read(self.program_counter);
+                let addr = pos.wrapping_add(self.register_x) as u16;
+                addr
+            }
+            AddressingMode::ZeroPage_Y => {
+                let pos = self.mem_read(self.program_counter);
+                let addr = pos.wrapping_add(self.register_y) as u16;
+                addr
+            }
+            AddressingMode::Absolute_X => {
+                let pos = self.mem_read_u16(self.program_counter);
+                let addr = pos.wrapping_add(self.register_x as u16);
+                addr
+            }
+            AddressingMode::Absolute_Y => {
+                let pos = self.mem_read_u16(self.program_counter);
+                let addr = pos.wrapping_add(self.register_y as u16);
+                addr
+            }
+            // https://zhuanlan.zhihu.com/p/458206025
+            // 先变址后间接
+            AddressingMode::Indirect_X => {
+                let base = self.mem_read(self.program_counter);
+
+                //变址
+                let ptr: u8 = (base as u8).wrapping_add(self.register_x);
+                //间接
+                let lo = self.mem_read(ptr as u16);
+                let hi = self.mem_read(ptr.wrapping_add(1) as u16);
+                (hi as u16) << 8 | (lo as u16)
+            }
+            // 先间接后变址
+            AddressingMode::Indirect_Y => {
+                let base = self.mem_read(self.program_counter);
+
+                //间接
+                let lo = self.mem_read(base as u16);
+                let hi = self.mem_read((base as u8).wrapping_add(1) as u16);
+                let deref_base = (hi as u16) << 8 | (lo as u16);
+                //变址
+                let deref = deref_base.wrapping_add(self.register_y as u16);
+                deref
+            }
+            AddressingMode::NoneAddressing => {
+                panic!("mode {:?} is not supported", mode);
+            }
+        }
+    }
+
     fn mem_read(&self, addr: u16) -> u8 {
         self.memory[addr as usize]
     }
@@ -64,7 +154,7 @@ impl CPU {
         self.memory[addr as usize] = data;
     }
 
-    fn mem_read_u16(&mut self, pos: u16) -> u16 {
+    fn mem_read_u16(&self, pos: u16) -> u16 {
         let lo = self.mem_read(pos) as u16;
         let hi = self.mem_read(pos + 1) as u16;
         (hi << 8) | (lo as u16)
@@ -113,15 +203,17 @@ impl CPU {
         self.register_x = self.register_a;
 
         //Affects Flags: N Z
-        //根据register_a改变status标志
+        //根据register_x改变status标志
         //update Zero Flag - if that value is zero, this flag will be set
         self.update_zero_flag(self.register_x);
 
-        //update Negative Flag - 根据register_a标志位设置status标志
+        //update Negative Flag - 根据register_x标志位设置status标志
         self.update_negative_flag(self.register_x);
     }
 
-    fn lda(&mut self, value: u8) {
+    fn lda(&mut self, mode: &AddressingMode) {
+        let addr = self.get_operand_address(mode);
+        let value = self.mem_read(addr);
         self.register_a = value;
 
         // http://www.6502.org/tutorials/6502opcodes.html#LDA
@@ -144,10 +236,10 @@ impl CPU {
 
     pub fn run(&mut self) {
         loop {
-            let opscode = self.mem_read(self.program_counter);
+            let code = self.mem_read(self.program_counter);
             self.program_counter += 1;
             // http://www.6502.org/tutorials/6502opcodes.html
-            match opscode {
+            match code {
                 0xE8 => {
                     self.inx();
                 }
@@ -155,11 +247,36 @@ impl CPU {
                     self.tax();
                 }
                 0xA9 => {
-                    //Mode: Immediate
-                    //读取opscode的下一条参数
-                    let param = self.mem_read(self.program_counter);
+                    self.lda(&AddressingMode::Immediate);
                     self.program_counter += 1;
-                    self.lda(param);
+                }
+                0xA5 => {
+                    self.lda(&AddressingMode::ZeroPage);
+                    self.program_counter += 1;
+                }
+                0xB5 => {
+                    self.lda(&AddressingMode::ZeroPage_X);
+                    self.program_counter += 1;
+                }
+                0xAD => {
+                    self.lda(&AddressingMode::Absolute);
+                    self.program_counter += 1;
+                }
+                0xBD => {
+                    self.lda(&AddressingMode::Absolute_X);
+                    self.program_counter += 1;
+                }
+                0xB9 => {
+                    self.lda(&AddressingMode::Absolute_Y);
+                    self.program_counter += 1;
+                }
+                0xA1 => {
+                    self.lda(&AddressingMode::Indirect_X);
+                    self.program_counter += 1;
+                }
+                0xB1 => {
+                    self.lda(&AddressingMode::Indirect_Y);
+                    self.program_counter += 1;
                 }
                 0x00 => {
                     return;
@@ -221,5 +338,15 @@ mod test {
         cpu.run();
 
         assert_eq!(cpu.register_x, 1)
+    }
+
+    #[test]
+    fn test_lda_from_memory() {
+        let mut cpu = CPU::new();
+        cpu.mem_write(0x10, 0x55);
+
+        cpu.load_and_run(vec![0xa5, 0x10, 0x00]);
+
+        assert_eq!(cpu.register_a, 0x55);
     }
 }
